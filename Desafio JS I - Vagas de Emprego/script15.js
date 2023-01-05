795519cd05e1214f2ccd3cb2fd7991292d695ee3
Mauricio Pocas <mbpocas@hotmail.com>
2023-01-04 22:00:18 -0300

    Desafio Final JS I

--- Desafio JS I - Vagas de Emprego/script15.js
+++ Desafio JS I - Vagas de Emprego/script15.js
@@ -1,8 +1,120 @@
 alert("*** Vagas de Emprego *** ");
 
+const vagas = [];
+
+const listJob = () => {
+  if (vagas.length == 0) {
+    return alert(`X Sem vagas disponíveis no momento. X`)
+  } else {
+  let textoVaga = ""
+  vagas.forEach((n, i) => {
+    textoVaga += `Vaga: (${i+1}) - ${n.nome} => Candidato(s): (${n.candidatos.length})\n`
+  })
+    return alert (`Vagas disponíveis:${vagas.length}\n
+${textoVaga}`)
+  }
+}
+
+function createJob() {
+  let newJob = {};
+  newJob.nome = prompt(`Informe o nome da vaga:`);
+  newJob.descricao = prompt(`Informe a descrição da vaga:`);
+  newJob.dataLimite = prompt(`Qual a data limite para inscrição: `);
+  newJob.candidatos = [];
+
+  let save = confirm(`Deseja salvar esta vaga? \n
+  Nome da Vaga: ${newJob.nome}
+  Descrição: ${newJob.descricao}
+  Data Limite: ${newJob.dataLimite}
+  `);
+
+  if (save) {
+    vagas.push(newJob);
+    alert(`Vaga salva com sucesso na base dados!`);
+  } else {
+    alert(`X Vaga não foi salva na base de dados X`);
+  }
+}
+
+const viewJob = () => {
+  if (vagas.length == 0) {
+    alert(`X Sem vagas disponíveis no momento. X`)
+  } else {
+  let textoCandidato  = ""
+  let indice = parseInt(prompt("Informe o índice da vaga que deseja exibir:"))
+  let vaga = vagas[indice - 1]
+
+    if (vaga != undefined) {
+      vaga.candidatos.map( candidato => {textoCandidato += `- ${candidato}\n`} )
+      alert
+          (`
+    Índice: ${indice}
+    Vaga: ${vaga.nome}
+    Descrição: ${vaga.descricao}
+    Data Limite: ${vaga.dataLimite}
+    Quantidade de Candidatos: ${vaga.candidatos.length}
+    Candidatos inscritos:\n${textoCandidato}`)
+    } else {
+      alert('Indice inválido')
+    }
+  }
+}
+
+const addUserJob = () => {
+  if (vagas.length == 0) {
+    alert(`X Sem vagas disponíveis no momento. X`)
+  } else {
+  let indice = parseInt(prompt("Informe o índice da vaga que deseja inscrever o candidato:"))
+  let vaga = vagas[indice - 1]
+
+    if (vaga != undefined) {
+      let username = prompt("Informe o nome do candidato para inscrição na vaga:")
+      let save = confirm(`Deseja inscrever o candidato "${username}" nesta vaga? \n
+    Nome da Vaga: ${vaga.nome}
+    Descrição: ${vaga.descricao}
+    Data Limite: ${vaga.dataLimite}
+    `)
+      if (save){
+        vaga.candidatos.push(username)
+        alert(`Inscrição realizada com sucesso!`)
+      } else {
+        alert(`X Candidato não foi inscrito na vaga selecionada X`)
+      }
+    } else {
+      alert('Indice inválido')
+    }
+  }
+}
+
+const deleteJob = () => {
+  if (vagas.length == 0) {
+    alert(`X Sem vagas disponíveis no momento. X`)
+  } else {
+  let indice = parseInt(prompt("Informe o índice da vaga que deseja excluir:"))
+  let vaga = vagas[indice - 1]
+
+    if (vaga != undefined) {
+      let deleteJob = confirm(`Deseja realmente excluir a vaga "${vaga.nome}"? \n
+    Nome da Vaga: ${vaga.nome}
+    Descrição: ${vaga.descricao}
+    Data Limite: ${vaga.dataLimite}
+    Quantidade de Candidatos: ${vaga.candidatos.length}
+    `)
+      if (deleteJob){
+        vagas.splice(indice -1 , 1)
+        console.log(vagas)
+        alert(`Vaga excluída com sucesso!`)
+      } else {
+        alert(`X Vaga não foi excluída da base de dados. X`)
+      }
+    } else {
+      alert('Indice inválido')
+    }
+  }
+}
 
 const displayMenu = () => {
-  return parseInt(prompt(`Calculadora Geométrica\n
+  return parseInt(prompt(`Vagas de Emprego\n
   1- Listar vagas disponíves
   2- Criar uma nova vaga
   3- Visualizar uma vaga
@@ -10,7 +122,6 @@ const displayMenu = () => {
   5- Excluir uma vaga
   6- Sair`))
 }
-
 
 const run = () => {
   let option = 0;
@@ -22,14 +133,19 @@ do {
 
   switch (option) {
     case 1:
+      listJob()
       break
     case 2:
+      createJob()
       break
     case 3:
+      viewJob()
       break
     case 4:
+      addUserJob()
       break
     case 5:
+      deleteJob()
       break
     case 6:
       alert("Saindo...")
@@ -42,6 +158,5 @@ do {
 
 
 } while (option !== 6)
-
 
 run()
